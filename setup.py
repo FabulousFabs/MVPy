cf7c9acc0425b9f1098bf910bfe4124de80d851d
Fabian Schneider <fabi.schn@gmail.com>
2025-02-26 09:08:53 +0100

    fix action

--- setup.py
+++ setup.py
@@ -4,5 +4,14 @@ setup(
     name="MVPy",
     version="0.1",
     packages=find_packages(),
-    install_requires=[],  # Add dependencies here if needed
+    install_requires=[
+        "numpy>=1.19.0",
+        "scipy>=1.10.1"
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=6.0.0",
+            "flake8>=3.8.0"
+        ]
+    },
 )
